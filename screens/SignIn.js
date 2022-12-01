fb4ae678b17bdd1647a5d83fc3c14c29ccce0145
unknown <dragontaek98@naver.com>
2022-12-01 16:02:57 +0900

    feat: spotify admin

--- screens/SignIn.js
+++ screens/SignIn.js
@@ -6,8 +6,37 @@ import {
   PrefaceContainer, PrefaceTitle, PrefaceContent,InnerText, ButtonContainer, ButtonContent, InputBackText, IconContent, ButtonInner
 } from '../theme/StyledSignInPage';
 import * as Progress from 'react-native-progress';
+import { SPOTIFY_CLIENT_ID, SPOTIFY_CLIENT_SECRET } from '@env';
+import { makeRedirectUri, useAuthRequest } from 'expo-auth-session';
+import { useEffect } from 'react';
+import * as WebBrowser from 'expo-web-browser';
+import * as Linking from 'expo-linking';
+
+WebBrowser.maybeCompleteAuthSession();
+
+const discovery = {
+  authorizationEndpoint: 'https://accounts.spotify.com/authorize',
+  tokenEndpoint: 'https://accounts.spotify.com/api/token',
+};
 
 export function SignIn({navigation, view}) {
+    const [request, response, promptAsync] = useAuthRequest(
+      {
+        clientId: `${SPOTIFY_CLIENT_ID}`,
+        scopes: ['user-read-email', 'playlist-modify-public','streaming'],
+        usePKCE: false,
+        redirectUri: Linking.createURL('SignIn'),
+      },
+      discovery
+    );
+    
+    useEffect(() => {
+      if (response?.type === 'success') {
+          const { code } = response.params;
+          // access code
+        }
+    }, [response]);
+
     return (
       <AuthContainer onLayout={view}>
         <Progress.Bar progress={0.3} width={360} color={'#E97777'}/>
@@ -27,7 +56,7 @@ export function SignIn({navigation, view}) {
           <InputContent placeholder="닉네임을 입력해주세요."></InputContent>
         </InputContainer>
         <ButtonContainer>
-          <ButtonContent>
+          <ButtonContent onPress={()=>promptAsync()}>
             <ButtonInner>
               <InnerText>
                 Spotify 연동하고 가입하기
