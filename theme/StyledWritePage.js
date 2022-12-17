ad05d15ecad36628a871c615ef09b74cf2a86467
2zzzi <2ghun1210@gmail.com>
2022-12-17 22:35:03 +0900

    add Result.js and styledWritePage.js

---
+++ theme/StyledWritePage.js
@@ -0,0 +1,76 @@
+import styled from 'styled-components/native';
+
+
+export const WriteBoxContainer = styled.View`
+    // box-sizing: border-box;
+    margin: auto;
+    align-items: center;
+    justify-content: center;
+    // border : 1px solid black;
+    
+`
+export const TitleBox = styled.View`
+box-sizing: border-box;
+margin: auto;
+
+`
+export const WriteTitle = styled.Text`
+    width: 45px;
+    height: 14px;
+
+    font-size: 12px;
+    line-height: 14px;
+    display: flex;
+    align-items: center;
+    color: #3F3D56;
+`
+export const WriteTitleBox = styled.TextInput`
+    width: 340px;
+    height: 38px;
+    background: #FFFFFF;
+    margin-top : 10px;
+    box-shadow: 0px 0px 10px rgba(154, 154, 154, 0.1);
+    border-radius: 10px;
+`
+export const ContentBox = styled.View`
+    box-sizing: border-box;
+    margin: auto;
+`
+export const WriteContent = styled.Text`
+    width: 45px;
+    height: 14px;
+
+    margin-top : 20px;
+
+    font-size: 12px;
+    line-height: 14px;
+    display: flex;
+    align-items: center;
+    color: #3F3D56;
+`
+export const WriteContentBox = styled.TextInput`
+    width: 340px;
+    height: 250px;
+    background: #FFFFFF;
+    box-shadow: 0px 0px 10px rgba(154, 154, 154, 0.1);
+    border-radius: 10px;
+    align-text :top;
+    margin-top : 10px;
+`
+export const CautionMent = styled.Text`
+    width: 340px;
+    height: 14px;
+    font-size: 12px;
+    line-height: 14px;
+    display: flex;
+    align-items: center;
+    text-align: right;
+    vertical-align : text-top;
+
+    color: #E97777;
+
+    // border : 1px solid black;
+    margin : auto;
+
+`
+
